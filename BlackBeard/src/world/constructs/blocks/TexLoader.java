a963be0ccc9f56768fc19c75b8ac9ba77a8f6964
some1ukno <63475220+some1ukno@users.noreply.github.com>
2020-05-13 15:41:22 -0700

    Add other textures into Texture Loader

--- BlackBeard/src/world/constructs/blocks/TexLoader.java
+++ BlackBeard/src/world/constructs/blocks/TexLoader.java
@@ -8,6 +8,15 @@ public class TexLoader {
 	public TexLoader() {
 		System.out.println("true! THIS METHOD HAS BEEN CALLED AT RUNTIME");
 		ObjectLoaderV_C.addLoaderHandleInstance("Client/Resources/Sailcloth/Sailcloth.png");
+		ObjectLoaderV_C.addLoaderHandleInstance("Client/Resources/Stone/Stone.png");
+		ObjectLoaderV_C.addLoaderHandleInstance("Client/Resources/Wood/Wood.png");
+		ObjectLoaderV_C.addLoaderHandleInstance("Client/Resources/Wood/Wood2.png");
+		ObjectLoaderV_C.addLoaderHandleInstance("Client/Resources/Grass/Grass.png");
+		ObjectLoaderV_C.addLoaderHandleInstance("Client/Resources/Grass/Grass1.png");
+		ObjectLoaderV_C.addLoaderHandleInstance("Client/Resources/Grass/Grass2.png");
+		ObjectLoaderV_C.addLoaderHandleInstance("Client/Resources/Grass/Grass3.png");
+		ObjectLoaderV_C.addLoaderHandleInstance("Client/Resources/Grass/Grass4.png");
+		ObjectLoaderV_C.addLoaderHandleInstance("Client/Resources/Grass/Grass5.png");
 	}
 
 }

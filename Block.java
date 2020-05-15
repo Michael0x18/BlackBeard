1dbc187f9cea0023793fbe1eba89f7518c3237c5
billmengg <50380093+billmengg@users.noreply.github.com>
2020-05-15 15:32:09 -0700

    kinda working select method
    Activates on click. Test by clicking the outer edge of the map (green parts)

---
+++ Block.java
@@ -0,0 +1,239 @@
+package world.constructs.blocks;
+
+import com.jogamp.opengl.GL;
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.glu.GLU;
+import com.jogamp.opengl.util.gl2.GLUT;
+
+import javafx.scene.paint.Color;
+import world.constants.declaration.ViewConstants;
+import world.viewport.JoglPane;
+import world.viewport.MVector;
+
+/**
+ * 
+ * @author Michael Ferolito Common superclass for all Blocks. When created,
+ *         forms a "GRASS BLOCK"
+ *
+ */
+public class Block implements Clippable {
+
+	public static Block fromDoubleCoords(double x2, double y2, double z2) {
+		return new Block(x2 / 2, y2 / 2, z2 / 2);
+
+	}
+
+	public void moveTo(double a, double b, double c) {
+		x=a;
+		y=b;
+		z=c;
+		mv = new MVector(x,y,z);
+	}
+
+	public static boolean TESSELATION_ON = false;
+
+	double x, y, z;
+	//	private double red = Math.random()*4;
+	//	private double green = Math.random()*4;
+	//	private double blue = Math.random()*4;
+	protected final double size = 0.5;
+
+	protected MVector mv;
+
+	protected boolean isSelected;
+
+	public Block() {
+		x = 0;
+		y = 0;
+		z = 0;
+		mv = new MVector(x, y, z);
+		isSelected = false;
+	}
+
+	public Block(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		mv = new MVector(x, y, z);
+		isSelected = false;
+	}
+
+	protected boolean outOfRange() {
+		//		if(MVector.add(mv.copy().mult(-1), JoglPane.currentLoader.getC().getPosition()).getMagnitude()>ViewConstants.ABSOLUTE_RENDER_RANGE) {
+		//			return true;
+		//		}
+		return false;
+	}
+
+	@Override
+	public void draw(GLUT glut, GLU glu, GL gl, GL2 gl2) {
+		if(outOfRange())
+			return;
+		gl2.glTranslated(x, y, z);
+		// glut.glutSolidCube((float) 0.5);
+		gl2.glColor3d(Color.LAWNGREEN.getRed(), Color.LAWNGREEN.getGreen(), Color.LAWNGREEN.getBlue());
+		if(isSelected) {
+			System.out.println("yay");
+			if (TESSELATION_ON) {
+				gl2.glTranslated(0.125, 0.125, 0.125);
+				glut.glutWireCube(0.25f);
+				gl2.glTranslated(-0.125, -0.125, -0.125);
+
+				gl2.glTranslated(-0.125, 0.125, 0.125);
+				glut.glutWireCube(0.25f);
+				gl2.glTranslated(0.125, -0.125, -0.125);
+
+				gl2.glTranslated(0.125, -0.125, 0.125);
+				glut.glutWireCube(0.25f);
+				gl2.glTranslated(-0.125, 0.125, -0.125);
+
+				gl2.glTranslated(0.125, 0.125, -0.125);
+				glut.glutWireCube(0.25f);
+				gl2.glTranslated(-0.125, -0.125, 0.125);
+
+				gl2.glTranslated(-0.125, -0.125, -0.125);
+				glut.glutWireCube(0.25f);
+				gl2.glTranslated(0.125, 0.125, 0.125);
+
+				gl2.glTranslated(-0.125, -0.125, 0.125);
+				glut.glutWireCube(0.25f);
+				gl2.glTranslated(0.125, 0.125, -0.125);
+
+				gl2.glTranslated(0.125, -0.125, -0.125);
+				glut.glutWireCube(0.25f);
+				gl2.glTranslated(-0.125, 0.125, 0.125);
+
+				gl2.glTranslated(-0.125, 0.125, -0.125);
+				glut.glutWireCube(0.25f);
+				gl2.glTranslated(0.125, -0.125, 0.125);
+			} else {
+				glut.glutWireCube(0.5f);
+			}
+		} else {
+			if (TESSELATION_ON) {
+				gl2.glTranslated(0.125, 0.125, 0.125);
+				glut.glutSolidCube(0.25f);
+				gl2.glTranslated(-0.125, -0.125, -0.125);
+
+				gl2.glTranslated(-0.125, 0.125, 0.125);
+				glut.glutSolidCube(0.25f);
+				gl2.glTranslated(0.125, -0.125, -0.125);
+
+				gl2.glTranslated(0.125, -0.125, 0.125);
+				glut.glutSolidCube(0.25f);
+				gl2.glTranslated(-0.125, 0.125, -0.125);
+
+				gl2.glTranslated(0.125, 0.125, -0.125);
+				glut.glutSolidCube(0.25f);
+				gl2.glTranslated(-0.125, -0.125, 0.125);
+
+				gl2.glTranslated(-0.125, -0.125, -0.125);
+				glut.glutSolidCube(0.25f);
+				gl2.glTranslated(0.125, 0.125, 0.125);
+
+				gl2.glTranslated(-0.125, -0.125, 0.125);
+				glut.glutSolidCube(0.25f);
+				gl2.glTranslated(0.125, 0.125, -0.125);
+
+				gl2.glTranslated(0.125, -0.125, -0.125);
+				glut.glutSolidCube(0.25f);
+				gl2.glTranslated(-0.125, 0.125, 0.125);
+
+				gl2.glTranslated(-0.125, 0.125, -0.125);
+				glut.glutSolidCube(0.25f);
+				gl2.glTranslated(0.125, -0.125, 0.125);
+			} else {
+				glut.glutSolidCube(0.5f);
+			}
+		}
+
+		// gl2.glColor3d(0, .5, 0);
+		// glut.glutWireCube(0.5001f);
+
+		gl2.glTranslated(-x, -y, -z);
+
+	}
+
+	/**
+	 * Check bounds Greater or less than no equal to.
+	 * 
+	 * @param x
+	 * @param y
+	 * @param z
+	 */
+	public boolean containsPoint(double x, double y, double z) {
+		// the x y z coords of the block are in the center so +/- by size/2 in all
+		// directions to get the edges
+		double left = this.x - size / 2;
+		double right = this.x + size / 2;
+		double top = this.y - size / 2;
+		double bottom = this.y + size / 2;
+		double front = this.z - size / 2;
+		double back = this.z + size / 2;
+		if (x >= left && x <= right && y >= top && y <= bottom && z >= front && z <= back) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public MVector getCoords() {
+		return new MVector(x, y, z);
+	}
+
+	@Override
+	public double[] packCoords() {
+		double[] a = { x, y, z };
+		return a;
+	}
+
+	public void select(boolean b) {
+		isSelected = b;
+	
+	}
+
+	@Override
+	public double getSize() {
+		return 0.5;
+	}
+
+	@Override
+	public double getX() {
+		return x;
+	}
+
+	@Override
+	public double getY() {
+		return y;
+	}
+
+	@Override
+	public double getZ() {
+		return z;
+	}
+
+	public boolean containsPoint1(double x, double y, double z) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void movex(double d) {
+		x+=d;
+
+	}
+
+	@Override
+	public void movey(double d) {
+		y+=d;
+
+	}
+
+	@Override
+	public void movez(double d) {
+		z+=d;
+
+	}
+
+}
---
+++ JoglPane.java
@@ -0,0 +1,536 @@
+package world.viewport;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import javax.swing.*;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+
+import com.jogamp.opengl.GL;
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLCapabilities;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.awt.GLJPanel;
+import com.jogamp.opengl.glu.GLU;
+import com.jogamp.opengl.util.gl2.GLUT; // for drawing the sample teapot
+
+import client.net.Client;
+import world.constructs.blocks.Clippable;
+import world.constructs.projectiles.Projectile;
+import world.constructs.OtherPlayer;
+import world.constructs.Ship;
+import world.constructs.StructPlayer;
+import world.ui.JoglMenu;
+import javafx.scene.paint.Color;
+
+/*glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	
+glMatrixMode(GL_PROJECTION );
+glLoadIdentity();
+glOrtho(0,1,0,1,-1,1);
+glDisable(GL_DEPTH_TEST);
+glDisable(GL_LIGHTING);
+glDepthMask(GL_FALSE);
+glMatrixMode(GL_MODELVIEW);
+glLoadIdentity();
+//draw 2D image
+glDepthMask(GL_TRUE);
+glEnable(GL_DEPTH_TEST);
+glEnable(GL_LIGHTING);
+glMatrixMode(GL_PROJECTION);
+glLoadIdentity();
+gluPerspective(45.0, (GLfloat) W_WIDTH / (GLfloat) W_HEIGHT, 0.1, 1000.0);
+glMatrixMode(GL_MODELVIEW);
+glLoadIdentity();
+
+
+
+
+/**
+ * Represents a panel for drawing. The bulk of the graphics is done here.
+ */
+public class JoglPane extends JPanel
+		implements GLEventListener, KeyListener, MouseListener, MouseMotionListener, ActionListener {
+	/**
+	 * UID- for dumping this into an off heap array??????
+	 */
+	private static final long serialVersionUID = 6999533994092038855L;
+	private static JFrame window;
+	private static Cursor blankCursor;
+//	private static JPanel pl;
+	private CopyOnWriteArrayList<Integer> Keys = new CopyOnWriteArrayList<Integer>();
+	private GLU glu = new GLU();
+//	private boolean isFullScreen;
+	public static JoglPane currentLoader;
+
+	/**
+	 * Called as if a main method.
+	 * 
+	 * @param flags
+	 */
+	public static void _driver_start(String[] flags) {
+		try {
+			UIManager.setLookAndFeel(new NimbusLookAndFeel());
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// Grid.loadTowerOfBabel();
+		Grid.load("TEMPLE.GRID");
+//		Ship sh = new Ship();
+
+		window = new JFrame("JOGL");
+		JoglPane j = new JoglPane();
+//		pl = new JPanel() {
+//			public void paintComponent(Graphics g) {
+//				this.setBackground(new java.awt.Color(0, 0, 0, 0));
+//				this.setOpaque(false);
+//				g.fillRect(0, 0, 100, 100);
+//			}
+//		};
+		// window.add(pl);
+
+		window.setContentPane(j);
+		window.pack();
+		window.setLocation(50, 50);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
+		// Transparent 16 x 16 pixel cursor image.
+		BufferedImage cursorImg = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+
+		// Create a new blank cursor.
+		blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(cursorImg, new java.awt.Point(0, 0),
+				"blank cursor");
+
+		// Set the blank cursor to the JFrame.
+		j.setCursor(blankCursor);
+		// window.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		// window.setCursor(new Cursor(0));
+
+	}
+
+	/**
+	 * yeets the cursor into oblivion
+	 */
+	public void cursorOff() {
+		this.setCursor(blankCursor);
+	}
+
+	/**
+	 * gets back your mouse
+	 */
+	public void cursorOn() {
+		this.setCursor(new Cursor(Cursor.MOVE_CURSOR));
+	}
+
+	private GLJPanel display;
+	Timer animationTimer;
+	private float rotateX, rotateY; // rotation amounts about axes, controlled
+									// by keyboard
+	private Player c;
+//	private SensorBlock sb;
+	private JMenuBar jmb;
+	private JMenu file;
+
+	/**
+	 * returns the player associated with this object. Weird dependencies are ok, as
+	 * are multiple JVM's.
+	 * 
+	 * @return
+	 */
+	public Player getC() {
+		return c;
+	}
+
+//	 public void paint(Graphics g){
+//	this.setBackground(new Color(0,0,0,0));
+//	this.setOpaque(false);
+//	g.fillRect(0, 0, 100, 100);
+//	 }
+
+	/**
+	 * new JoglPane. Really only should be used from within this class.
+	 */
+	JoglPane() {
+		currentLoader = this;
+		GLCapabilities caps = new GLCapabilities(null);
+		display = new GLJPanel(caps);
+		display.setPreferredSize(new Dimension(600, 600));
+		// size here
+		display.addGLEventListener(this);
+		setLayout(new BorderLayout());
+		add(display, BorderLayout.CENTER);
+
+		rotateX = 0; // initialize some variables used in the drawing.
+		rotateY = 0;
+
+		requestFocusInWindow();
+		display.addKeyListener(this);
+
+		// handling
+		display.addMouseListener(this);
+		display.addMouseMotionListener(this);
+
+		// c = new Camera();
+		// c.setScale(3);
+		// c.installTrackball(this);
+		// c.setScale(3);
+		// Ship sh = new Ship();
+		// c = new Player(this,sh);
+		c = new Player(this);
+		c.setScale(0.6);
+		c.moveTo(0, 5, 0);
+//		sb = new SensorBlock(c, 1, 0, 0);
+		jmb = JoglMenu.applyMenu(window);
+		file = new JMenu("File");
+		jmb.add(file);
+		startAnimation();
+
+	}
+
+	// --------------- Methods of the GLEventListener interface -----------
+
+	private GLUT glut = new GLUT(); // for drawing the teapot
+	private double xt;
+	/**
+	 * true if escape has been pressed.
+	 */
+	boolean disabled;
+	private boolean fullscreen;
+
+	/**
+	 * This method is called when the OpenGL display needs to be redrawn.
+	 */
+	public void display(GLAutoDrawable drawable) {
+		if (!ObjectLoaderV_C.loaded) {
+			return;
+		}
+		GL2 gl = drawable.getGL().getGL2();
+		gl.glClearColor((float) Color.LIGHTSKYBLUE.getRed(), (float) Color.LIGHTSKYBLUE.getGreen(),
+				(float) Color.LIGHTSKYBLUE.getBlue(), 0);
+
+		gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+		gl.glLoadIdentity(); // Set up modelview transform.
+		c.act(Grid.world);
+		c.draw(gl, this, window);
+		gl.glTranslated(xt, 0, 0);
+		gl.glRotatef(rotateY, 0, 1, 0);
+		gl.glRotatef(rotateX, 1, 0, 0);
+
+		GL2 gl2 = gl.getGL2();
+
+		for (Clippable c : Grid.world) {
+			c.draw(glut, glu, gl, gl.getGL2());
+		}
+		gl.glColor4d(Color.ROYALBLUE.getRed(), Color.ROYALBLUE.getGreen(), Color.ROYALBLUE.getBlue(), 0.5);
+		gl.glTranslated(0, -50.5, 0);
+		glut.glutSolidCube(100);
+		gl.glTranslated(0, 50.5, 0);
+		for (Ship s : Grid.ships) {
+			s.draw(glut, glu, gl, gl2);
+		}
+		for (StructPlayer p : Grid.players) {
+			OtherPlayer.draw(p, glut, glu, gl, gl2);
+		}
+		for(Projectile p : Grid.shots) {
+			p.draw(glut, glu, gl, gl2);
+		}
+
+		gl.glEnable(GL2.GL_LIGHTING);
+		gl.glEnable(GL2.GL_LIGHT0);
+		gl.glEnable(GL2.GL_NORMALIZE);
+		// float[] position = { 0, 20, 0, 1 };
+		float[] position = { (float) c.getPosition().x, (float) c.getPosition().y, (float) c.getPosition().z, 1 };
+		gl2.glLightfv(GL2.GL_LIGHT0, GL2.GL_POSITION, position, 0);
+
+		gl2.glEnable(GL2.GL_LIGHT0);
+
+	}
+
+	/**
+	 * This is called when the GLJPanel is first created. It can be used to
+	 * initialize the OpenGL drawing context.
+	 */
+	public void init(GLAutoDrawable drawable) {
+		// called when the panel is created
+		GL2 gl = drawable.getGL().getGL2();
+		ObjectLoaderV_C.load(gl);
+		gl.glClearColor(0.8F, 0.8F, 0.8F, 1.0F);
+		gl.glEnable(GL.GL_DEPTH_TEST);
+		gl.glEnable(GL2.GL_LIGHTING);
+		gl.glEnable(GL2.GL_LIGHT0);
+		gl.glEnable(GL2.GL_COLOR_MATERIAL);
+
+	}
+
+	/**
+	 * Called when the size of the GLJPanel changes. Note:
+	 * glViewport(x,y,width,height) has already been called before this method is
+	 * called!
+	 */
+	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+	}
+
+	/**
+	 * This is called before the GLJPanel is destroyed. It can be used to release
+	 * OpenGL resources.
+	 */
+	public void dispose(GLAutoDrawable drawable) {
+	}
+
+	// ------------ Support for keyboard handling ------------
+
+	/**
+	 * Called when the user presses any key.
+	 */
+	public void keyPressed(KeyEvent e) {
+
+//		if (e.getModifiersEx() !=0)
+//			return;
+		// Keys.add(Integer.valueOf(e.getKeyCode()));
+		if (!Keys.contains(e.getKeyCode())) {
+			Keys.add(Integer.valueOf(e.getKeyCode()));
+		}
+		repaint();
+
+	}
+
+	/**
+	 * Called when the user types a character.
+	 */
+	public void keyTyped(KeyEvent e) {
+		// char ch = e.getKeyChar(); // Which character was typed.
+	}
+
+	public void keyReleased(KeyEvent e) {
+
+		if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+			c.getRobot().mouseMove(window.getX() + this.getBounds().width / 2,
+					window.getY() + this.getBounds().height / 2);
+			c.clearMouseStats();
+			disabled = !disabled;
+			if (this.getCursor().equals(blankCursor))
+				this.cursorOn();
+			else
+				this.cursorOff();
+
+		} else if (e.getKeyCode() == KeyEvent.VK_F) {
+			if (fullscreen) {
+				fullscreen = false;
+				window.dispose();
+				window.setUndecorated(false);
+				window.pack();
+				window.setBounds(0, 0, 800, 600);
+				window.setJMenuBar(jmb);
+				window.setVisible(true);
+			} else {
+				fullscreen = true;
+				window.dispose();
+				window.setUndecorated(true);
+				window.pack();
+				window.setBounds(0, 0, Toolkit.getDefaultToolkit().getScreenSize().width,
+						Toolkit.getDefaultToolkit().getScreenSize().height);
+				window.setJMenuBar(jmb);
+				window.setVisible(true);
+			}
+		} else if (e.getKeyCode() == KeyEvent.VK_G)
+			c.toggleGravity();
+		while (Keys.contains(Integer.valueOf(e.getKeyCode())))
+			Keys.remove(Integer.valueOf(e.getKeyCode()));
+		repaint();
+	}
+
+	// --------------------------- animation support ---------------------------
+
+	int frameNumber = 0;
+
+	private boolean animating;
+
+	/**
+	 * called each frame.
+	 */
+	private void updateFrame() {
+		// window.setLayout(null);
+		frameNumber++;
+		// this.setBounds(-300,-300,window.getWidth()+600,window.getHeight()+600);
+
+	}
+
+	/**
+	 * starts the animation timer
+	 */
+	public void startAnimation() {
+		if (!animating) {
+			if (animationTimer == null) {
+				animationTimer = new Timer(40, this);
+			}
+			animationTimer.start();
+			animating = true;
+		}
+	}
+
+	/**
+	 * stops the animation timer
+	 */
+	public void pauseAnimation() {
+		if (animating) {
+			animationTimer.stop();
+			animating = false;
+		}
+	}
+
+	/**
+	 * NO CALL!
+	 */
+	public void actionPerformed(ActionEvent evt) {
+		updateFrame();
+		int i = 0;
+		for (Integer key : Keys) {
+			// System.out.println(e);
+			if (key == KeyEvent.VK_ESCAPE) {
+				break;
+			}
+
+			if (key == KeyEvent.VK_W)
+				c.moveZ(1);
+			else if (key == KeyEvent.VK_S)
+				c.moveZ(-1);
+			else if (key == KeyEvent.VK_A)
+				c.moveX(1);
+			else if (key == KeyEvent.VK_D)
+				c.moveX(-1);
+			else if (key == KeyEvent.VK_ESCAPE)
+				i = 1;
+			else if (key == KeyEvent.VK_SPACE)
+				c.jump();
+			else if (key == KeyEvent.VK_R)
+				Grid.load("TEMPLE.GRID");
+			else if (key == KeyEvent.VK_LEFT) {
+				if (c.lastShip != null)
+					Client.listener.sendMessafe(":port " + c.lastShip.name);
+			} else if (key == KeyEvent.VK_RIGHT) {
+				if (c.lastShip != null)
+					Client.listener.sendMessafe(":star " + c.lastShip.name);
+			} else if (key == KeyEvent.VK_UP) {
+				if (c.lastShip != null)
+					Client.listener.sendMessafe(":acel " + c.lastShip.name);
+			} else if (key == KeyEvent.VK_DOWN) {
+				if (c.lastShip != null)
+					Client.listener.sendMessafe(":deac " + c.lastShip.name);
+			}
+
+			if (i == 1) {
+				break;
+			}
+		}
+		display.repaint();
+		repaint();
+	}
+
+	// ---------------------- support for mouse events ----------------------
+
+	private boolean dragging; // is a drag operation in progress?
+
+	@SuppressWarnings("unused")
+	private int startX, startY; // starting location of mouse during drag
+	@SuppressWarnings("unused")
+	private int prevX, prevY; // previous location of mouse during drag
+
+	/**
+	 * Called when the user presses a mouse button on the display.
+	 */
+	public void mousePressed(MouseEvent evt) {
+		if (dragging) {
+			return; // don't start a new drag while one is already in progress
+		}
+		int x = evt.getX();
+		int y = evt.getY();
+		dragging = true; // might not always be correct!
+		prevX = startX = x;
+		prevY = startY = y;
+		display.repaint(); // only needed if display should change
+	}
+
+	/**
+	 * Called when the user releases a mouse button after pressing it on the
+	 * display.
+	 */
+	public void mouseReleased(MouseEvent evt) {
+		if (!dragging) {
+			return;
+		}
+		dragging = false;
+
+	}
+
+	/**
+	 * Called during a drag operation when the user drags the mouse on the display/
+	 */
+	public void mouseDragged(MouseEvent evt) {
+		if (!dragging) {
+			return;
+		}
+		int x = evt.getX();
+		int y = evt.getY();
+		// TODO: respond to mouse drag to new point (x,y)
+		prevX = x;
+		prevY = y;
+		display.repaint();
+	}
+
+	/**
+	 * Don't call.
+	 */
+	public void mouseMoved(MouseEvent evt) { // System.out.println("moved");
+		// c.ship.sail();
+	}
+
+	/**
+	 * Dont call
+	 */
+	public void mouseClicked(MouseEvent evt) {
+		if (evt.getButton() == (MouseEvent.BUTTON1)) {
+			//Client.listener.sendMessafe(":shootEvent");
+			c.selection();
+		}
+//		try {
+//			FileWrapper fr = new FileWrapper("TEMPLE.GRID");
+//			String str = "";
+//			while(fr.ready()){
+//				str += fr.readLine()+"\n";
+//			}
+//			fr.close();
+//			Printer out = new Printer("TEMPLE.GRID");
+//			out.print(str);
+//			int x = (int)(2*(c.position.x+Math.cos(c.getPan()-1))+0.5);
+//			int y = (int)(c.position.y*2);
+//			int z = (int)(2*(c.position.z+Math.sin(c.getPan()))+0.5);
+//			out.print("Block\t\t"+x+"\t"+y+"\t"+z+"\n");
+//			out.close();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+	}
+
+	/**
+	 * dont call
+	 */
+	public void mouseEntered(MouseEvent evt) {
+	}
+
+	/**
+	 * don't cal;
+	 */
+	public void mouseExited(MouseEvent evt) {
+		// c.getRobot().mouseMove(window.getX()+getWidth()/2,
+		// window.getY()+getWidth()/2);
+	}
+
+//	public static void main(String[] args) {
+//		_driver_start(args);
+//
+//	}
+
+}

ba0a39c803b66efb61237510667fda22318f2093
MichaelOnBlue <michaelonblue@Ferolitos-MBP.attlocal.net>
2020-05-15 18:06:27 -0700

    I needed this for the graphics stuff. I chose to include it.

---
+++ BlackBeard/src/DatColor.java
@@ -0,0 +1,12 @@
+import java.awt.Color;
+
+import javax.swing.JColorChooser;
+import javax.swing.JFrame;
+
+public class DatColor {
+	
+	public static void main(String[] args) {
+		JColorChooser.showDialog(new JFrame(), "Pick Dat Color", Color.BLACK);
+	}
+	
+}

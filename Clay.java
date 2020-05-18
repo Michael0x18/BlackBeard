dbb723427d73230c2bcabef3e23567fe744e3a0a
billmengg <50380093+billmengg@users.noreply.github.com>
2020-05-18 15:45:16 -0700

    Add files via upload
    finished selection and working of block placing

---
+++ Clay.java
@@ -0,0 +1,101 @@
+package world.constructs.blocks;
+
+import com.jogamp.opengl.GL;
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.glu.GLU;
+import com.jogamp.opengl.util.gl2.GLUT;
+
+import javafx.scene.paint.Color;
+
+/**
+ * Represents a red clay block. Clay supports tesselation factors.
+ * 
+ * @author Michael Ferolito
+ * @version 2
+ * @since 2
+ */
+public class Clay extends Block {
+
+	/**
+	 * super()
+	 */
+	public Clay() {
+		super();
+	}
+
+	/**
+	 * super
+	 * @param x2
+	 * @param y2
+	 * @param z2
+	 * @return
+	 */
+	public static Clay fromDoubleCoords(double x2, double y2, double z2) {
+		return new Clay(x2 / 2, y2 / 2, z2 / 2);
+
+	}
+
+	/**
+	 * super
+	 * @param i
+	 * @param j
+	 * @param k
+	 */
+	public Clay(double i, double j, double k) {
+		super(i, j, k);
+	}
+
+	/**
+	 * super, but red.
+	 */
+	public void draw(GLUT glut, GLU glu, GL gl, GL2 gl2) {
+		if (outOfRange())
+			return;
+		gl2.glTranslated(x, y, z);
+		// glut.glutSolidCube((float) 0.5);
+		gl2.glColor3d(Color.CRIMSON.getRed(), Color.CRIMSON.getGreen(), Color.CRIMSON.getBlue());
+		if (TESSELATION_ON) {
+			gl2.glTranslated(0.125, 0.125, 0.125);
+			glut.glutSolidCube(0.25f);
+			gl2.glTranslated(-0.125, -0.125, -0.125);
+
+			gl2.glTranslated(-0.125, 0.125, 0.125);
+			glut.glutSolidCube(0.25f);
+			gl2.glTranslated(0.125, -0.125, -0.125);
+
+			gl2.glTranslated(0.125, -0.125, 0.125);
+			glut.glutSolidCube(0.25f);
+			gl2.glTranslated(-0.125, 0.125, -0.125);
+
+			gl2.glTranslated(0.125, 0.125, -0.125);
+			glut.glutSolidCube(0.25f);
+			gl2.glTranslated(-0.125, -0.125, 0.125);
+
+			gl2.glTranslated(-0.125, -0.125, -0.125);
+			glut.glutSolidCube(0.25f);
+			gl2.glTranslated(0.125, 0.125, 0.125);
+
+			gl2.glTranslated(-0.125, -0.125, 0.125);
+			glut.glutSolidCube(0.25f);
+			gl2.glTranslated(0.125, 0.125, -0.125);
+
+			gl2.glTranslated(0.125, -0.125, -0.125);
+			glut.glutSolidCube(0.25f);
+			gl2.glTranslated(-0.125, 0.125, 0.125);
+
+			gl2.glTranslated(-0.125, 0.125, -0.125);
+			glut.glutSolidCube(0.25f);
+			gl2.glTranslated(0.125, -0.125, 0.125);
+		} else {
+			glut.glutSolidCube(0.5f);
+		}if(super.isSelected) {
+			glut.glutWireCube(0.5f);
+		}
+//		gl2.glColor3d(0, .5, 0);
+//		glut.glutWireCube(0.5001f);
+
+		gl2.glTranslated(-x, -y, -z);
+
+	}
+
+}
---
+++ Stone.java
@@ -0,0 +1,148 @@
+package world.constructs.blocks;
+
+import com.jogamp.opengl.GL;
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.glu.GLU;
+import com.jogamp.opengl.util.gl2.GLUT;
+
+import javafx.scene.paint.Color;
+import world.viewport.ObjectLoaderV_C;
+
+/**
+ * Plain gray stone.
+ * @author Michael Ferolito
+ *
+ */
+public class Stone extends Block {
+	static boolean fancy = false;
+	
+	/**
+	 * see super factory
+	 * @param x2
+	 * @param y2
+	 * @param z2
+	 * @return
+	 */
+	public static Stone fromDoubleCoords(double x2, double y2, double z2) {
+		return new Stone(x2/2,y2/2,z2/2);
+		
+	}
+
+	/**
+	 * see super default constructor
+	 */
+	public Stone() {
+		super();
+	}
+
+	/**
+	 * see super constructor
+	 * @param x
+	 * @param y
+	 * @param z
+	 */
+	public Stone(double x, double y, double z) {
+		super(x, y, z);
+	}
+	
+	/**
+	 * draw method.
+	 */
+	public void draw(GLUT glut, GLU glu, GL gl, GL2 gl2) {
+		if(outOfRange())
+			return;
+		gl2.glTranslated(x, y, z);
+		// glut.glutSolidCube((float) 0.5);
+		gl2.glColor4d(Color.LIGHTSLATEGRAY.getRed(), Color.LIGHTSLATEGRAY.getGreen(), Color.LIGHTSLATEGRAY.getBlue(),4);
+//		
+//		if(TESSELATION_ON) {
+//		gl2.glTranslated(0.125, 0.125, 0.125);
+//		glut.glutSolidCube(0.25f);
+//		gl2.glTranslated(-0.125, -0.125, -0.125);
+//		
+//		gl2.glTranslated(-0.125, 0.125, 0.125);
+//		glut.glutSolidCube(0.25f);
+//		gl2.glTranslated(0.125, -0.125, -0.125);
+//		
+//		gl2.glTranslated(0.125, -0.125, 0.125);
+//		glut.glutSolidCube(0.25f);
+//		gl2.glTranslated(-0.125, 0.125, -0.125);
+//		
+//		gl2.glTranslated(0.125, 0.125, -0.125);
+//		glut.glutSolidCube(0.25f);
+//		gl2.glTranslated(-0.125, -0.125, 0.125);
+//		
+//		gl2.glTranslated(-0.125, -0.125, -0.125);
+//		glut.glutSolidCube(0.25f);
+//		gl2.glTranslated(0.125, 0.125, 0.125);
+//		
+//		gl2.glTranslated(-0.125, -0.125, 0.125);
+//		glut.glutSolidCube(0.25f);
+//		gl2.glTranslated(0.125, 0.125, -0.125);
+//		
+//		gl2.glTranslated(0.125, -0.125, -0.125);
+//		glut.glutSolidCube(0.25f);
+//		gl2.glTranslated(-0.125, 0.125, 0.125);
+//		
+//		gl2.glTranslated(-0.125, 0.125, -0.125);
+//		glut.glutSolidCube(0.25f);
+//		gl2.glTranslated(0.125, -0.125, 0.125);
+//		}else {
+//			glut.glutSolidCube(0.5f);
+//		}
+		
+		//gl2.glColor3d(0, .5, 0);
+		//glut.glutWireCube(0.5001f);
+		if(fancy) {
+		gl.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_WRAP_S, GL.GL_REPEAT);
+		gl.glBindTexture(GL2.GL_TEXTURE_2D, ObjectLoaderV_C.loaderHash.get("Client/Resources/Stone/Stone.png").getTextureObject(gl));
+	    gl2.glBegin(GL2.GL_QUADS);
+
+	    // Front Face
+	    gl2.glTexCoord2f(0.0f, 0.0f); gl2.glVertex3f(-0.25f, -0.25f, 0.25f);
+	    gl2.glTexCoord2f(0.25f, 0.0f); gl2.glVertex3f( 0.25f, -0.25f, 0.25f);
+	    gl2.glTexCoord2f(0.25f, 0.25f); gl2.glVertex3f( 0.25f, 0.25f, 0.25f);
+	    gl2.glTexCoord2f(0.0f, 0.25f); gl2.glVertex3f(-0.25f, 0.25f, 0.25f);
+
+	    // Back Face
+	    gl2.glTexCoord2f(0.25f, 0.0f); gl2.glVertex3f(-0.25f, -0.25f, -0.25f);
+	    gl2.glTexCoord2f(0.25f, 0.25f); gl2.glVertex3f(-0.25f, 0.25f, -0.25f);
+	    gl2.glTexCoord2f(0.0f, 0.25f); gl2.glVertex3f( 0.25f, 0.25f, -0.25f);
+	    gl2.glTexCoord2f(0.0f, 0.0f); gl2.glVertex3f( 0.25f, -0.25f, -0.25f);
+
+	    // Top Face
+	    gl2.glTexCoord2f(0.0f, 0.25f); gl2.glVertex3f(-0.25f, 0.25f, -0.25f);
+	    gl2.glTexCoord2f(0.0f, 0.0f); gl2.glVertex3f(-0.25f, 0.25f, 0.25f);
+	    gl2.glTexCoord2f(0.25f, 0.0f); gl2.glVertex3f( 0.25f, 0.25f, 0.25f);
+	    gl2.glTexCoord2f(0.25f, 0.25f); gl2.glVertex3f( 0.25f, 0.25f, -0.25f);
+
+	    // Bottom Face
+	    gl2.glTexCoord2f(0.25f, 0.25f); gl2.glVertex3f(-0.25f, -0.25f, -0.25f);
+	    gl2.glTexCoord2f(0.0f, 0.25f); gl2.glVertex3f( 0.25f, -0.25f, -0.25f);
+	    gl2.glTexCoord2f(0.0f, 0.0f); gl2.glVertex3f( 0.25f, -0.25f, 0.25f);
+	    gl2.glTexCoord2f(0.25f, 0.0f); gl2.glVertex3f(-0.25f, -0.25f, 0.25f);
+	    
+	    // Right face
+	    gl2.glTexCoord2f(0.25f, 0.0f); gl2.glVertex3f( 0.25f, -0.25f, -0.25f);
+	    gl2.glTexCoord2f(0.25f, 0.25f); gl2.glVertex3f( 0.25f, 0.25f, -0.25f);
+	    gl2.glTexCoord2f(0.0f, 0.25f); gl2.glVertex3f( 0.25f, 0.25f, 0.25f);
+	    gl2.glTexCoord2f(0.0f, 0.0f); gl2.glVertex3f( 0.25f, -0.25f, 0.25f);
+
+	    // Left Face
+	    gl2.glTexCoord2f(0.0f, 0.0f); gl2.glVertex3f(-0.25f, -0.25f, -0.25f);
+	    gl2.glTexCoord2f(0.25f, 0.0f); gl2.glVertex3f(-0.25f, -0.25f, 0.25f);
+	    gl2.glTexCoord2f(0.25f, 0.25f); gl2.glVertex3f(-0.25f, 0.25f, 0.25f);
+	    gl2.glTexCoord2f(0.0f, 0.25f); gl2.glVertex3f(-0.25f, 0.25f, -0.25f);
+	    gl2.glEnd();
+	    gl2.glFlush();
+		}else {
+			glut.glutSolidCube(0.5f);
+		}if(super.isSelected) {
+			glut.glutWireCube(0.5f);
+		}
+		
+		gl2.glTranslated(-x, -y, -z);
+
+	}
+
+}
